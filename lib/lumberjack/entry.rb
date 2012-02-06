d01028673e6141625ce98b6355047031d678bcd2
Michael Wagner <mwagner@digitalinsites.com>
2012-02-06 11:15:49 -0600

    added method for outputting entry as hash

--- lib/lumberjack/entry.rb
+++ lib/lumberjack/entry.rb
@@ -12,9 +12,19 @@ module Lumberjack
 
     end
 
+    def to_hash
+      { :address   => address,
+        :hostname  => hostname,
+        :port      => port,
+        :pri       => pri,
+        :timestamp => timestamp,
+        :message   => message }
+    end
+
     def to_string
       "<#{pri}> #{timestamp} #{hostname} #{message}"
     end
+
 
   end
 end
